e6d74b95656307c2a2a38db8492fbe00b5696fb9
Verity <verity.boyd@edu.sait.ca>
2026-02-02 16:32:30 -0700

    changed and added styling

--- src/ui/components/BodyBattery.tsx
+++ src/ui/components/BodyBattery.tsx
@@ -7,30 +7,73 @@ export default function BodyBattery() {
         <Text style={styles.title}>In Focus</Text>
       </View>
       <View style={styles.card}>
+        <View style={styles.heading}>
+          <Image
+            style={styles.batteryIcon}
+            source={require("../../../assets/images/batteryicon.png")}
+          />
+          <Text style={styles.subtitle}>Body Battery</Text>
+
+          <View>
+            <Text style={styles.mainScore}>51</Text>
+          </View>
+
+          <View style={styles.scores}>
+            <View style={styles.charged}>
+              <Text style={styles.scoresNum}>+63</Text>
+              <Text style={styles.scoresText}>Charged</Text>
+            </View>
+            <View style={styles.charged}>
+              <Text style={styles.scoresNum}>-32</Text>
+              <Text style={styles.scoresText}>Drained</Text>
+            </View>
+          </View>
+        </View>
         <Image
-          style={styles.batteryIcon}
-          source={require("../../../assets/images/batteryicon.png")}
+          style={styles.graph}
+          source={require("../../../assets/images/graph.png")}
         />
-        <Text>Body Battery</Text>
-        <Text style={styles.batteryScore}>51</Text>
-        <Text>+63</Text>
-        <Text>Charged</Text>
-        <Text>-32</Text>
-        <Text>Drained</Text>
-        <Image source={require("../../../assets/images/graph.png")} />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  subtitle: {
+    marginTop: 15,
+    marginLeft: 5,
+    fontSize: 15,
+  },
+  mainScore: {
+    fontSize: 35,
+  },
+  charged: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  scores: {
+    marginTop: 50,
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: "space-around",
+  },
+  scoresNum: {
+    fontSize: 20,
+  },
+  scoresText: {
+    fontSize: 15,
+    color: "#ABABAB",
+  },
+  heading: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+  },
   card: {
     backgroundColor: "#FFFFFF",
-    width: 350,
-    height: 250,
-    borderRadius: 20,
+    width: 395,
+    height: 355,
+    borderRadius: 15,
     padding: 10,
-    gap: 5,
   },
   title: {
     fontSize: 20,
@@ -42,13 +85,9 @@ const styles = StyleSheet.create({
     width: 45,
     height: 45,
   },
-  batteryScore: {
-    fontSize: 30,
-  },
-  chargedScore: {
-    fontSize: 20,
-  },
-  drainedScore: {
-    fontSize: 20,
+  graph: {
+    marginTop: 15,
+    height: 200,
+    width: 385,
   },
 });
