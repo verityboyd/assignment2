b974cd29fef3e756aaf2f99afab090d0d6383f8d
Aurora Choban <aurorachoban@gmail.com>
2026-02-02 11:44:49 -0700

    ActionBar Complete

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import ActionBar from "../src/ui/components/ActionBar/ActionBar";
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <ActionBar />
     </View>
   );
 }
