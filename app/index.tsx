8f84c12d19639c419b59dbe8d4c169dd268e40ea
Aurora Choban <aurorachoban@gmail.com>
2026-02-02 18:15:38 -0700

    created navbar

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,20 @@
 import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import ActionBar from "../src/ui/components/ActionBar/ActionBar";
+import NavBar from "../src/ui/components/NavBar/NavBar";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <ActionBar />
+    <View style={{ flex: 1 }}>
+      <SafeAreaView edges={["top"]}>
+        <ActionBar />
+      </SafeAreaView>
+
+      <View style={{ flex: 1 }} />
+
+      <SafeAreaView edges={["bottom"]}>
+        <NavBar />
+      </SafeAreaView>
     </View>
   );
 }
